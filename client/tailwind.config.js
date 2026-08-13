1deffea7df76849c5ab19df1f05a9eade62d8704
rampoz27 <swordmiracle@gmail.com>
2026-08-13 13:59:06 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,17 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        hacker: {
-          bg: '#0a0a0a',
-          card: '#121212',
-          border: '#00ff66',
-          green: '#00ff66',
-          darkgreen: '#003311'
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
