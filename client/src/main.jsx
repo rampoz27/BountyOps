534b7560d8936c8b7aa5b727ec94dad4065671b0
rampoz27 <swordmiracle@gmail.com>
2026-08-13 13:44:46 +0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // Import CSS hacker tema gelap hijau neon di sini
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
